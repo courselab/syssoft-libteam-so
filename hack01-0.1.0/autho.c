981872a3b7bd182c041ab8ad28e55e8072f16935
leonrossi <leo.rossi@usp.br>
2023-07-15 18:11:51 -0300

    cracked code

---
+++ hack01-0.1.0/autho.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void authorize();
+
+int main() {
+    authorize();
+    return 0;
+}
---
+++ hack01-0.1.0/authorize.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void authorize() {
+    printf("Signal worked!\n");
+    return;
+}
